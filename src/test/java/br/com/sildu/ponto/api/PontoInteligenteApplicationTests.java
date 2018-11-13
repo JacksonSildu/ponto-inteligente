f8453027bf4ac799774a5b651037720af4304ecd
Jackson Sildu <jackson.sildu@gmail.com>
2018-11-12 22:44:20 -0200

    Incluindo dados de testes.

--- src/test/java/br/com/sildu/ponto/api/PontoInteligenteApplicationTests.java
+++ src/test/java/br/com/sildu/ponto/api/PontoInteligenteApplicationTests.java
@@ -3,10 +3,14 @@ package br.com.sildu.ponto.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles(value = "integrationtest")
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class PontoInteligenteApplicationTests {
 
 	@Test
