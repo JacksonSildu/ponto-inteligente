73253749396d13f094e8f4e55cfc431a111aa668
Jackson Sildu <jackson.sildu@gmail.com>
2018-11-15 13:47:38 -0200

    Adicionando security

---
+++ src/main/java/br/com/sildu/ponto/api/controllers/CadastroPFController.java
@@ -0,0 +1,132 @@
+package br.com.sildu.ponto.api.controllers;
+
+import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sildu.ponto.api.dtos.CadastroPFDto;
+import br.com.sildu.ponto.api.entities.Empresa;
+import br.com.sildu.ponto.api.entities.Funcionario;
+import br.com.sildu.ponto.api.enums.PerfilEnum;
+import br.com.sildu.ponto.api.response.Response;
+import br.com.sildu.ponto.api.services.EmpresaService;
+import br.com.sildu.ponto.api.services.FuncionarioService;
+import br.com.sildu.ponto.api.utils.PasswordUtils;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/cadastrar-pf")
+public class CadastroPFController {
+
+	private static final Logger log = LoggerFactory.getLogger(CadastroPFController.class);
+
+	@Autowired
+	private EmpresaService empresaService;
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	/**
+	 * Cadastra um funcionário pessoa física no sistema.
+	 * 
+	 * @param cadastroPFDto
+	 * @param result
+	 * @return ResponseEntity<Response<CadastroPFDto>>
+	 * @throws NoSuchAlgorithmException
+	 */
+	@PostMapping
+	public ResponseEntity<Response<CadastroPFDto>> cadastrar(@Valid @RequestBody CadastroPFDto cadastroPFDto, BindingResult result) {
+		log.info("Cadastrando PF: {}", cadastroPFDto);
+		Response<CadastroPFDto> response = new Response<>();
+
+		validarDadosExistentes(cadastroPFDto, result);
+		Funcionario funcionario = this.converterDtoParaFuncionario(cadastroPFDto);
+
+		if (result.hasErrors()) {
+			log.error("Erro validando dados de cadastro PF: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		Optional<Empresa> empresa = this.empresaService.buscarPorCnpj(cadastroPFDto.getCnpj());
+		empresa.ifPresent(funcionario::setEmpresa);
+		this.funcionarioService.persistir(funcionario);
+
+		response.setData(this.converterCadastroPFDto(funcionario));
+		return ResponseEntity.ok(response);
+	}
+
+	/**
+	 * Verifica se a empresa está cadastrada e se o funcionário não existe na base de dados.
+	 * 
+	 * @param cadastroPFDto
+	 * @param result
+	 */
+	private void validarDadosExistentes(CadastroPFDto cadastroPFDto, BindingResult result) {
+		Optional<Empresa> empresa = this.empresaService.buscarPorCnpj(cadastroPFDto.getCnpj());
+		if (!empresa.isPresent()) {
+			result.addError(new ObjectError("empresa", "Empresa não cadastrada."));
+		}
+
+		this.funcionarioService.buscarPorCpf(cadastroPFDto.getCpf()).ifPresent(func -> result.addError(new ObjectError("funcionario", "CPF já existente.")));
+
+		this.funcionarioService.buscarPorEmail(cadastroPFDto.getEmail()).ifPresent(func -> result.addError(new ObjectError("funcionario", "Email já existente.")));
+	}
+
+	/**
+	 * Converte os dados do DTO para funcionário.
+	 * 
+	 * @param cadastroPFDto
+	 * @param result
+	 * @return Funcionario
+	 * @throws NoSuchAlgorithmException
+	 */
+	private Funcionario converterDtoParaFuncionario(CadastroPFDto cadastroPFDto) {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome(cadastroPFDto.getNome());
+		funcionario.setEmail(cadastroPFDto.getEmail());
+		funcionario.setCpf(cadastroPFDto.getCpf());
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt(cadastroPFDto.getSenha()));
+		cadastroPFDto.getQtdHorasAlmoco().ifPresent(qtdHorasAlmoco -> funcionario.setQtdHorasAlmoco(Float.valueOf(qtdHorasAlmoco)));
+		cadastroPFDto.getQtdHorasTrabalhoDia().ifPresent(qtdHorasTrabDia -> funcionario.setQtdHorasTrabalhoDia(Float.valueOf(qtdHorasTrabDia)));
+		cadastroPFDto.getValorHora().ifPresent(valorHora -> funcionario.setValorHora(new BigDecimal(valorHora)));
+
+		return funcionario;
+	}
+
+	/**
+	 * Popula o DTO de cadastro com os dados do funcionário e empresa.
+	 * 
+	 * @param funcionario
+	 * @return CadastroPFDto
+	 */
+	private CadastroPFDto converterCadastroPFDto(Funcionario funcionario) {
+		CadastroPFDto cadastroPFDto = new CadastroPFDto();
+		cadastroPFDto.setId(funcionario.getId());
+		cadastroPFDto.setNome(funcionario.getNome());
+		cadastroPFDto.setEmail(funcionario.getEmail());
+		cadastroPFDto.setCpf(funcionario.getCpf());
+		cadastroPFDto.setCnpj(funcionario.getEmpresa().getCnpj());
+		funcionario.getQtdHorasAlmocoOpt().ifPresent(qtdHorasAlmoco -> cadastroPFDto.setQtdHorasAlmoco(Optional.of(Float.toString(qtdHorasAlmoco))));
+		funcionario.getQtdHorasTrabalhoDiaOpt().ifPresent(qtdHorasTrabDia -> cadastroPFDto.setQtdHorasTrabalhoDia(Optional.of(Float.toString(qtdHorasTrabDia))));
+		funcionario.getValorHoraOpt().ifPresent(valorHora -> cadastroPFDto.setValorHora(Optional.of(valorHora.toString())));
+
+		return cadastroPFDto;
+	}
+
+}
---
+++ src/main/java/br/com/sildu/ponto/api/security/dto/JwtAuthenticationDto.java
@@ -0,0 +1,20 @@
+package br.com.sildu.ponto.api.security.dto;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+public class JwtAuthenticationDto {
+
+	@NotEmpty(message = "Email não pode ser vazio.")
+	@Email(message = "Email inválido.")
+	private String email;
+
+	@NotEmpty(message = "Senha não pode ser vazia.")
+	private String senha;
+
+}
